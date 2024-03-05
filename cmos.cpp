ccb2f14055b227cb8224a748675d0046e2a8c21c
Jansen Craft <89416543+jc103820@users.noreply.github.com>
2024-03-04 19:04:40 -0500

    Add functional winnowing function

--- cmos.cpp
+++ cmos.cpp
@@ -12,50 +12,62 @@
 
 using namespace std;
 
-vector<string> tokens;
-vector<size_t> hashes;
-vector<pair<int,size_t>> record;
+vector<pair<size_t,int>> winnow (int w, const vector<size_t>& get_hash){
+    deque<size_t> buffer(w, SIZE_T_MAX);
+    vector<pair<size_t,int>> fingerprints;
+    int min_hash_index = 0;
 
-void winnow (int w){
-    deque<size_t> buffer(w, INT_MAX);
-    int min_hash_index = w;
+    // Load initial k grams
+    for (int k_idx = 0; k_idx < w-1; k_idx++){
+        buffer.push_back(get_hash[k_idx]);
+        buffer.pop_front();
+        min_hash_index--;
+    }
 
-    for (int k_idx = 0; k_idx < hashes.size(); k_idx++){
-        buffer.push_back(hashes[k_idx]);
+    for (int k_idx = w-1; k_idx < get_hash.size(); k_idx++){
+        buffer.push_back(get_hash[k_idx]);
         buffer.pop_front();
         min_hash_index--;
         
         if (min_hash_index == -1){
+            min_hash_index = w-1;
             for(int i = w-1; i >= 0; i--)
                 if (buffer[i] < buffer[min_hash_index]) min_hash_index = i;
-            record.push_back({k_idx, buffer[min_hash_index]});
+            fingerprints.push_back({buffer[min_hash_index],k_idx - (w-1-min_hash_index)});
         } else {
             if (buffer.back() <= buffer[min_hash_index]){
                 min_hash_index = w-1;
-                record.push_back({k_idx, buffer[min_hash_index]});
+                fingerprints.push_back({buffer[min_hash_index],k_idx});
             }
         }
     }
+    return fingerprints;
 }
 
 int main(){
     string token, tmp;
-    int k = 10,w = 8;
+    int k = 5, w = 4;
     hash<string> magic_hash_machine;
-    while(cin >> token) tokens.push_back(token);
+    vector<string> tokens;
 
-    hashes.resize(tokens.size()-k);
+    while(cin >> token) tokens.push_back(token);
+    vector<size_t> kgram_hash(tokens.size()-k);
 
     for (int i = 0; i < tokens.size()-k; i++){
         for (int j = i; j < i + k; j++)
             tmp += tokens[j];
-        hashes[i] = magic_hash_machine(tmp);
+        kgram_hash[i] = magic_hash_machine(tmp);
     }
 
-    winnow(w);
+    cout << "\n\n" << kgram_hash.size() << '\n';
 
-    for(auto h: record)
-        cout << h.first << '\t' << h.second << '\n';
+    // vector<size_t> kgram_hash2 = {77,74,42,17,98,50,17,98,8,88,67,39,77,74,42,17,98};
+    // auto fingerprints = winnow(w,kgram_hash2);
     
-    cout << "\n\n" << record.size() << '\n';
+    auto fingerprints = winnow(w,kgram_hash);
+
+    for(auto fp: fingerprints)
+        cout << fp.first << '\t' << fp.second << '\n';
+    
+    cout << "\n\n" << fingerprints.size() << '\n';
 }
