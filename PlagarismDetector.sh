9aeaf56d0e14b1c94856c5e08893c6e9d0e40240
Jansen Craft <89416543+jc103820@users.noreply.github.com>
2024-03-06 11:37:36 -0500

    Add file redirection to bash script

--- PlagarismDetector.sh
+++ PlagarismDetector.sh
@@ -3,17 +3,17 @@ FILES=./"$1"/*
 dirEntered="$1"
 len=${#dirEntered}
 len=$(($len+3))
-rm tokens.txt
+rm tokens.txt scanner_out.txt
 for f in $FILES
 do
-  echo "Tokenizing ${f:len} file..."
-  # take action on each file. $f store current file name
-	./scanner < $f
-	echo -ne "${f:len}">>tokens.txt
-	echo -ne " ">>tokens.txt
+  	# take action on each file. $f store current file name
+  	echo "Tokenizing ${f:len} file..."
+	./scanner < $f >> scanner_out.txt
+	echo -ne "${f:len}" >> tokens.txt
+	echo -ne " " >> tokens.txt
 	cat scanner_out.txt >> tokens.txt
-	echo "">>tokens.txt
+	echo "" >> tokens.txt
 done
 echo "Detecting Plagarism, this may take a few minutes."
-./cmos > PlagarismReport.txt
+./cmos < tokens.txt > PlagarismReport.txt
 echo "Done. Results stored in PlagarismReport.txt"
